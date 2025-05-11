064549fc8b5dc05d24fb1b37efabb1cd5f6e4e1f
oscarprdev <vlcoscar8@gmail.com>
2025-05-11 13:53:07 +0200

    feat: implement LogOutButton component with presentation and functionality

---
+++ src/components/LogOutButton/LogOutButtonPresentation.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { LogoutButtonPresentationEmits } from './LogOutButtonPresentation.types';
+
+const emit = defineEmits<LogoutButtonPresentationEmits>();
+</script>
+<template>
+  <provet-button
+    data-testid="logout-button"
+    variant="primary"
+    @click="emit('click')"
+  >
+    <provet-icon
+      slot="start"
+      name="interface-logout"
+    ></provet-icon>
+    Logout
+  </provet-button>
+</template>
